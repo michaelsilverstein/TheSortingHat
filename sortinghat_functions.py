63884467f1f092e38c900a0ee87c6e7a48795acc
Michael Silverstein <msilverstein@finchtherapeutics.com>
2018-11-06 16:16:11 -0500

    house for functions

---
+++ sortinghat_functions.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+
+def generate_feature(means, stds, n, labels, seed=None):
+    """
+    Generate feature data assuming features are normally distributed.
+    For each input parameter, either a list (associated with the order of `labels`) or a single value can be passed. 
+        Passing a single value will assume that all classes share that parameter
+    Inputs:
+    | means: <list> or <float> Mean(s) of feature distribution
+    | stds: <list> or <float> Standard deviation(s) of feature distribution
+    | n: <list> or <float> Number of samples for each class
+    | labels: <array> List of labels (this order is associated with the other passed parameters)
+    | seed: <int> Random seed for sampling
+    Output:
+    | data: <n x 2 array> Data for single feature where each sample is labeled by class
+    """
+    # If any inputs are single values, convert them to lists
+    if np.isscalar(means):
+        means = [means]*len(labels)
+    if np.isscalar(stds):
+        stds = [stds]*len(labels)
+    if np.isscalar(n):
+        n = [n]*len(labels)
+    # Assign parameters to each class
+    params = {label: {'mean': m, 'std': s, 'n': size} for label, m, s, size in zip(labels, means, stds, n)}
+    
+    # Generate data
+    if seed:
+        np.random.seed(seed)
+    ## For each class, sample `n` points from a normal distribution with that classes mean and standard deviation
+    data = [[x, label] for label in labels for x in np.random.normal(params[label]['mean'], params[label]['std'], params[label]['n'])]
+    return data
